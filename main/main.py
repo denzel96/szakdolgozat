73a6ff0c9408fc10d634a0b9db05e211b61694a7
Dániel Szabó <szabo.daniel1996@gmail.com>
2019-04-03 21:53:56 +0200

    still trying

--- main/main.py
+++ main/main.py
@@ -1,8 +1,19 @@
 from github import Github
 
-g = Github('7d0107588539fb77a9710176f92e55eb70290881')
+g = Github('81a93fa01e526bacf1f8b1f93706bfc50c14b0cf')
 
-repo = g.get_repo("denzel96/copper")
-c = repo.get_commits()
-print(c)
-print(c.get_page(0)[0].commit.message)
+repoList = g.get_repos()
+# c = g.get_repos().totalCount
+# print('Num of repos: ', c)
+for i in range(0, 1000):
+    print(i, ' ', len(g.get_repos().get_page(i)))
+# for repo in repoList:
+#     c = repo.get_commits()
+#     print(len(c.get_page(2)))
+
+    # for i in range(0, len(a)):
+    #     print(c)
+    #     print(c.get_page(i))
+        # print(c.get_page(0)[0])
+        # print(c.get_page(0)[0].commit)
+        # print(c.get_page(0)[0].commit.message)
