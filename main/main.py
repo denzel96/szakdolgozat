34e19ea970203d833fa25bc1cd430f40edb3c6a7
Dániel Szabó <szabo.daniel1996@gmail.com>
2019-05-01 18:03:14 +0200

    git diff managing recognising old and new versions/changes

--- main/main.py
+++ main/main.py
@@ -1,4 +1,6 @@
+# TODO diff lines starting with 'import' should be ignored
 from github import Github
+import requests
 
 
 def get_related_commits(repo_path):
@@ -22,11 +24,44 @@ def get_message(commit):
         return commit.commit.message
 
 
-def get_diffs(repo, commit):
-    size = len(commit.parents)
-    for i in range(size):
-        return repo.compare(commit.commit.sha,
-                            commit.parents[i].sha).diff_url
+def get_diff(repo, commit):
+    this = commit.commit
+    parent = commit.parents[0]
+
+    cmp = repo.compare(parent.sha, this.sha)
+    diff = requests.get(cmp.diff_url)
+    diff_text = diff.text
+    parse_diff(diff_text)
+
+
+def parse_diff(diff_text):
+    body = diff_text.split('@@', 1)
+    infos = body[0]
+    versions = process_diff_item(body[1].split('@@')[1])
+    print('uj verzio:')
+    print(versions[0])
+    print('/////////////////////////////////')
+    print('regi verzio:')
+    print(versions[1])
+
+
+def process_diff_item(item):
+    lines = item.split('\n')
+    new_version = []
+    old_version = []
+
+    for line in lines:
+        if line.startswith('-'):
+            old_version.append(line.split('-')[1])
+        elif line.startswith('+'):
+            new_version.append(line.split('+')[1])
+        else:
+            old_version.append(line[1:])
+            new_version.append(line[1:])
+
+    new_text = '\n'.join(new_version)
+    old_text = '\n'.join(old_version)
+    return [new_text, old_text]
 
 
 ##########
@@ -35,9 +70,9 @@ pw = '|)aneeka:)XD11'
 g = Github('denzel96', pw)
 
 codeBase = [
-    'TucoBenedictoPacificoJuanMariaRamirez/fotav'
+    # 'TucoBenedictoPacificoJuanMariaRamirez/fotav'
     # ,
-    # 'Microsoft/pyright'
+    'Microsoft/pyright'
     # ,
     # 'achael/eht-imaging',
     # 'tensorflow/models',
@@ -46,9 +81,11 @@ codeBase = [
 
 changes = []
 for link in codeBase:
+    # print('Loading commits for ' + link)
     current_repo = g.get_repo(link)
     commits = get_related_commits(link)
-    i=9
-    msg = get_message(commits[i])
-    print(msg)
-    print(get_diffs(current_repo, commits[i]))
+    # print('Commits loaded\n\n\n')
+
+    # print('Message: ' + get_message(commits[0]))
+    # print('end msg')
+    get_diff(current_repo, commits[0])
